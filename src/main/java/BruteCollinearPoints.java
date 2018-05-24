60930db22a87e3d4550126b28e1083129859bf1f
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2018-05-24 19:26:00 +0200

    adjust configuration

--- src/main/java/BruteCollinearPoints.java
+++ src/main/java/BruteCollinearPoints.java
@@ -1,27 +1,100 @@
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
+
+import java.util.*;
+
 import edu.princeton.cs.algs4.In;
 
 public class BruteCollinearPoints {
-    private final Point[] p;
+    private final LineSegment[] lineSegments;
+
     // finds all line segments containing 4 points
     public BruteCollinearPoints(Point[] points) {
-        this.p = new Point[points.length];
-        for (int i = 0; i > points.length; i++) {
-            this.p[i] = points[i];
+        if (points == null) {
+            throw new java.lang.IllegalArgumentException("arguments is null");
+        }
+        int lng = points.length,
+            segmentsCount = 0;
+
+        Map<Double, Integer[]> slopes = new HashMap<Double, Integer[]>();
+        Set<String> setA = new HashSet<String>();
+
+        LineSegment[] lines = new LineSegment[lng];
+
+        for (int i = 0; i < lng; i++) {
+            if (points[i] == null) {
+                throw new java.lang.IllegalArgumentException("point item is null");
+            }
+            String key = points[i].toString();
+            if (setA.contains(key)) {
+                throw new java.lang.IllegalArgumentException("duplicated point");
+            } else {
+                setA.add(key);
+            }
+            
+            for (int j = i+1; j < lng; j++) {
+                double slope = points[i].slopeTo(points[j]);
+                
+                if (slopes.containsKey(key)) {
+                    
+                } else {
+
+                }
+                
+                //Point[] sPoints = new Point[lng*lng];
+            }
+            
+            // Point[] sPoints = new Point[lng*lng];
+            // int z = 0;
+            // for (int j = i+1; j < lng; j++) {
+            //     for (int x = j+1; x < lng - 1; x++) {
+            //         if (points[i].slopeTo(points[j]) == points[j].slopeTo(points[x])) {
+            //         if (!m.containsKey(i)) {
+            //             int[] anArray;
+            //             anArray[0] = 1;
+            //             m.put(i, anArray);
+            //         }
+            //             sPoints[z] = points[i];
+            //             sPoints[z+1] = points[j];
+            //             sPoints[z+2] = points[x];
+            //             z += 3;
+            //         }
+            //     }
+            // }
+
+//            if (z > 3) {
+//                int min = 0,
+//                    max = 0;
+//                for (int k = 1; k < z; k++) {
+//                    if (sPoints[k].compareTo(sPoints[min]) > 0) {
+//                        min = k;
+//                    }
+//                    if (sPoints[k].compareTo(sPoints[max]) < 0) {
+//                        max = k;
+//                    }
+//                }
+//
+//                lines[segmentsCount++] = new LineSegment(
+//                    sPoints[min],
+//                    sPoints[max]
+//                );
+//            }
+        }
+
+        lineSegments = new LineSegment[segmentsCount];
+        for (int i = 0; i < segmentsCount; i++) {
+            lineSegments[i] = lines[i];
         }
     }
 
     // the number of line segments
     public int numberOfSegments() {
-        return 1;
+        return lineSegments.length;
     }
 
     // the line segments
     public LineSegment[] segments() {
-        LineSegment[] lines = new LineSegment[this.p.length];
-
-        return lines;
+        return lineSegments;
     }
 
     public static void main(String[] args) {
@@ -50,6 +123,6 @@ public class BruteCollinearPoints {
         StdOut.println(segment);
         segment.draw();
     }
-    StdDraw.show();
+        StdDraw.show();
     }
  }
